2048d9fbee9c2dbcb566e1cc67645af088bb6c22
Steven Jarmell <sjj27@pitt.edu>
2022-08-11 19:42:45 -0400

    Fix comparison operator

--- src/Components/BoardSquare.tsx
+++ src/Components/BoardSquare.tsx
@@ -16,7 +16,7 @@ const BoardSquare = ({ letter, solution, showColor, listID }: Props) => {
 		<p className="board-square noselect">{letter}</p>
 	);
 	*/
-	return solutionArray.indexOf(letter) == listID && showColor ? (
+	return solutionArray.indexOf(letter) === listID && showColor ? (
 		<p className="board-square noselect correct-letter-space">{letter}</p>
 	) : solutionArray.includes(letter) && showColor ? (
 		<p className="board-square noselect valid-letter">{letter}</p>
