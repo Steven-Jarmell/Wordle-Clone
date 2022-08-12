65b826aaca48bb7ed202b210e4f10c020b73fdfb
Steven Jarmell <sjj27@pitt.edu>
2022-08-12 11:43:23 -0400

    Fix one bug in letter coloring

--- src/Components/BoardRow.tsx
+++ src/Components/BoardRow.tsx
@@ -14,7 +14,7 @@ const BoardRow = ({ solution, guess, showColor }: Props) => {
 	return (
 		<div className="board-row">
 			{cells.map((c, i) => (
-				<BoardSquare key={i} listID ={i} letter={c} solution={solution} showColor={showColor}/>
+				<BoardSquare key={i} listID ={i} letter={c} solution={solution} showColor={showColor} guess={guess}/>
 			))}
 		</div>
 	);
--- src/Components/BoardSquare.tsx
+++ src/Components/BoardSquare.tsx
@@ -5,20 +5,44 @@ type Props = {
 	solution: string;
 	showColor: boolean;
 	listID: number;
+	guess: string;
 };
 
-const BoardSquare = ({ letter, solution, showColor, listID }: Props) => {
+const BoardSquare = ({ letter, solution, showColor, listID, guess }: Props) => {
 	let solutionArray = solution.split("");
-	/*
-	return solutionArray.includes(letter) && showColor ? (
-		<p className="board-square noselect valid-letter">{letter}</p>
-	) : (
-		<p className="board-square noselect">{letter}</p>
-	);
-	*/
-	return solutionArray.indexOf(letter) === listID && showColor ? (
+	let guessArray = guess.split("");
+
+	const checkIfCorrect = () => {
+		for (let i = 0; i < solutionArray.length; i++) {
+			if (solutionArray[i] === letter && i === listID) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	const checkIfValid = () => {
+		return solutionArray.includes(letter);
+	}
+
+	const guessHasMoreThanOneButSolutionHasOne = () => {
+		let solutionCount = 0;
+		let guessCount = 0;
+		for (let i = 0; i < solutionArray.length; i++) {
+			if (solutionArray[i] === letter ) {
+				solutionCount++;
+			}
+			if (guess[i] === letter ) {
+				guessCount++;
+			}
+		}
+
+		return guessCount > solutionCount;
+	}
+
+	return checkIfCorrect() && showColor ? (
 		<p className="board-square noselect correct-letter-space">{letter}</p>
-	) : solutionArray.includes(letter) && showColor ? (
+	) : checkIfValid() && !(guessHasMoreThanOneButSolutionHasOne()) && showColor ? (
 		<p className="board-square noselect valid-letter">{letter}</p>
 	) : (
 		<p className="board-square noselect">{letter}</p>
